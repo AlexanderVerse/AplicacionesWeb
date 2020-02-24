9d57b2e5315745c0ca9267f3c794d30f19a8823d
Alexander <alexander.rangel@alumno.buap.mx>
2020-02-23 23:17:24 -0600

    Actualización de la página validacionPassword
    El código se optimizó para que sea mas comprensible en un futuro o cualquier otro usuario.
    También se optimizó de forma que sea mas rápido a la hora de ejecución.

--- JS/verificarPassword.js
+++ JS/verificarPassword.js
@@ -6,8 +6,8 @@ var botonValidar = document.getElementById("validar")
 var contrasenaComparar = document.getElementById("contrasenaComparar")
 var contrasena = document.getElementById("contrasena")
 var texto = document.getElementById("texto")
-var regexMayuscula = /[A-Z]+/g;
-var regexMinuscula = /[a-z]+/g;
+var regexMayuscula = /[A-Z]/g;
+var regexMinuscula = /[a-z]/g;
 var regexNumero = /[0-9]/g;
 var regexCaracter = /[\/\+\*\.\-\_\#\$\%\&]/g;
 var totalMayuscula = 3
@@ -25,86 +25,82 @@ inicializar = () => {
 }
 
 verificarSeguridadPassword = () =>{
-    let match = contrasena.value.match(regexMayuscula)
-    if (match != null && comprobarCantidadCoincidencia(match, totalMayuscula))
+    mensajePassword = ""
+    bloquesColor = 4
+    if (comprobarCantidadCoincidencia(regexMayuscula, contrasena.value, totalMayuscula) && comprobarCantidadCoincidencia(regexMinuscula, contrasena.value, totalMinuscula) && comprobarCantidadCoincidencia(regexNumero, contrasena.value, totalNumero) && comprobarCantidadCoincidencia(regexCaracter, contrasena.value, totalCaracter))
     {
-        match = contrasena.value.match(regexMinuscula)
-        if (match != null && comprobarCantidadCoincidencia(match, totalMinuscula))
-        {
-            match = contrasena.value.match(regexNumero)
-            if (match != null && comprobarCantidadCoincidencia(match, totalNumero))
-            {
-                match = contrasena.value.match(regexCaracter)
-                if (match != null && comprobarCantidadCoincidencia(match, totalCaracter))
-                {
-                    console.log("Verificada")
-                    colorBarraValidacion("green", 4)
-                    botonValidar.disabled = false
-                    contrasenaComparar.disabled = false
-                    texto.innerHTML = "La contraseña es valida"
-                }
-                else
-                {
-                    
-                    colorBarraValidacion("yellow", 3)
-                    contrasenaComparar.value = ""
-                    contrasenaComparar.disabled = true
-                    botonValidar.disabled = true
-                    texto.innerHTML = "La contraseña debe contener al menos " + totalCaracter + " caracter: [/, +, *, ., -, _, #, $, %, &]"
-                }
-            }
-            else
-            {
-                colorBarraValidacion("orange", 2)
-                botonValidar.disabled = true
-                contrasenaComparar.value = ""
-                contrasenaComparar.disabled = true
-                texto.innerHTML = "La contraseña debe contener al menos " + totalNumero + " números y " + totalCaracter + " carácteres."
-            }
-        }
-        else
-        {
-            
-            colorBarraValidacion("red", 1)
-            botonValidar.disabled = true
-            contrasenaComparar.value = ""
-            contrasenaComparar.disabled = true
-            texto.innerHTML = "La contraseña debe contener al menos " + totalMinuscula + " minúsculas, " + totalNumero + " números y " + totalCaracter + " carácteres."
-        }
+        console.log("Verificada")
+        colorBarraValidacion(bloquesColor)
+        botonValidar.disabled = false
+        contrasenaComparar.disabled = false
+        texto.innerHTML = "La contraseña es valida"
     }
     else
     {
-        colorBarraValidacion("white", 0)
-        botonValidar.disabled = true
+        if (!comprobarCantidadCoincidencia(regexMayuscula, contrasena.value, totalMayuscula))
+        {
+            mensajePassword += "<br> Debe contener al menos 3 mayúsculas "
+            bloquesColor -= 1
+        }
+        if (!comprobarCantidadCoincidencia(regexMinuscula, contrasena.value, totalMinuscula))
+        {
+            mensajePassword += "<br> Debe contener al menos 3 minúsculas "
+            bloquesColor -= 1
+        }
+        if(!comprobarCantidadCoincidencia(regexNumero, contrasena.value, totalNumero))
+        {
+            mensajePassword += "<br> Debe contener al menos 3 números "
+            bloquesColor -= 1
+        }
+        if(!comprobarCantidadCoincidencia(regexCaracter, contrasena.value, totalCaracter))
+        {
+            mensajePassword += "<br> Debe contener al menos 3 caracteres : [/, +, *, ., -, _, #, $, %, &]"
+            bloquesColor -= 1
+        }
+        colorBarraValidacion(bloquesColor)
         contrasenaComparar.value = ""
         contrasenaComparar.disabled = true
-        texto.innerHTML = "La contraseña debe contener al menos " + totalMayuscula + " mayúsculas, " + totalMinuscula + " minúsculas, " + totalNumero + " números y " + totalCaracter + " carácteres."
+        texto.innerHTML = "La contraseña debe contener al menos :" + mensajePassword
+        botonValidar.disabled = true
     }
 }
 
-colorBarraValidacion = (colorBarra, posicionBarra) =>
+colorBarraValidacion = (bloquesPintar) =>
 {
-    arregloColorBarra = ["white", "white", "white", "white"]
-    for (let index = 0; index < posicionBarra; index++)
+    validacionRojo.style.backgroundColor = "white"
+    validacionAnaranjado.style.backgroundColor = "white"
+    validacionAmarillo.style.backgroundColor = "white"
+    validacionVerde.style.backgroundColor = "white"
+    if (bloquesPintar === 1)
     {
-        arregloColorBarra[index] = colorBarra
+        validacionRojo.style.backgroundColor = "red"
     }
-    validacionRojo.style.backgroundColor = arregloColorBarra[0]
-    validacionAnaranjado.style.backgroundColor = arregloColorBarra[1]
-    validacionAmarillo.style.backgroundColor = arregloColorBarra[2]
-    validacionVerde.style.backgroundColor = arregloColorBarra[3]
+    else if(bloquesPintar === 2)
+    {
+        validacionRojo.style.backgroundColor = "orange"
+        validacionAnaranjado.style.backgroundColor = "orange"
+    }
+    else if(bloquesPintar === 3)
+    {
+        validacionRojo.style.backgroundColor = "yellow"
+        validacionAnaranjado.style.backgroundColor = "yellow"
+        validacionAmarillo.style.backgroundColor = "yellow"
+    }
+    else if(bloquesPintar === 4)
+    {
+        validacionRojo.style.backgroundColor = "green"
+        validacionAnaranjado.style.backgroundColor = "green"
+        validacionAmarillo.style.backgroundColor = "green"
+        validacionVerde.style.backgroundColor = "green"
+    }
 }
 
-comprobarCantidadCoincidencia = (arregloCoincidencia, cantidadCaracter) =>
+comprobarCantidadCoincidencia = (regex, cadenaEvaluar, cantidadCaracter) =>
 {
-    totalCaracterValidado = 0
-    for (let index = 0; index < arregloCoincidencia.length; index++)
+    let match = cadenaEvaluar.match(regex)
+    if (match != null && match.length >= cantidadCaracter)
     {
-        totalCaracterValidado += arregloCoincidencia[index].length
-        if (totalCaracterValidado >= cantidadCaracter)
-        {
-            return true
-        }
+        return true
     }
     return false
 }
